ba40fa193eb04d2dcffe602b5f5968874a69181f
mohie93 <mohie_93@hotmail.com>
2021-03-29 16:59:19 +0800

    changed greeting message

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hello World!
+          Hello World, From Supahands
         </a>
       </header>
     </div>
