dfeb29a6fc998ca8c7515963b1847db189df55bd
mohie93 <mohie_93@hotmail.com>
2021-04-01 13:22:56 +0800

    Code style refactoring

--- src/App.js
+++ src/App.js
@@ -7,12 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <a
-          className="App-link"
-          href="https://supahands.ai/careers"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://supahands.ai/careers" target="_blank" rel="noopener noreferrer">
           Hello From Supahands
         </a>
         <p>Branch is : {branchName}</p>
